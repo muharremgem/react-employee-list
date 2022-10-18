f14fad23c7b91f9072015ff958beeda54668e579
muharrem gem <muharremgem28@gmail.com>
2022-10-18 13:40:29 +0300

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import style from "./Header.module.scss";
 const Header = ({ indexOfFirstUser, indexOfLastUser }) => {
   return (
     <div>
-      <h3 className={style.heading}>Employee List</h3>
+      <h3 className={style.heading}>EMPLOYEE LİST</h3>
       <h5 className={style.count}>
         (Employees {indexOfFirstUser < 1 ? null : indexOfFirstUser} to{" "}
         {indexOfLastUser})
